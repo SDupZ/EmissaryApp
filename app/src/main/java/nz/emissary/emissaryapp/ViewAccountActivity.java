143a89ce64f3adb0294a6e0b584baa19fe8c204d
Simon du Preez <smdupnz@yahoo.co.nz>
2016-03-03 22:35:20 +1300

    Added viewaccountactivity back

---
+++ app/src/main/java/nz/emissary/emissaryapp/ViewAccountActivity.java
@@ -0,0 +1,49 @@
+package nz.emissary.emissaryapp;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v7.widget.AppCompatButton;
+import android.view.View;
+import android.widget.TextView;
+
+/**
+ * Created by Simon on 3/03/2016.
+ */
+public class ViewAccountActivity extends BaseActivity {
+
+    TextView usernameView;
+    TextView emailView;
+    AppCompatButton logoutButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        usernameView = (TextView) findViewById(R.id.username);
+        emailView = (TextView) findViewById(R.id.email);
+        logoutButton = (AppCompatButton) findViewById(R.id.logout_button);
+
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // FLAG_ACTIVITY_CLEAR_TASK only works on API 11, so if the user
+                // logs out on older devices, we'll just exit.
+                /*if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+                    Intent intent = new Intent(ViewAccountActivity.this,
+                            HomeActivity.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK
+                            | Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(intent);
+                } else {
+                    finish();
+                }*/
+            }
+        });
+    }
+
+    @Override
+    protected int getLayoutResource() {
+        return R.layout.activity_view_account;
+    }
+}
