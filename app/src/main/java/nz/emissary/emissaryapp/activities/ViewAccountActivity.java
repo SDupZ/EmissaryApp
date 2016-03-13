59d4fc41141fb90ed3ced2765d16848004aa2e09
Simon du Preez <smdupnz@yahoo.co.nz>
2016-03-13 21:32:52 +1300

    Added stuff to view account page

--- app/src/main/java/nz/emissary/emissaryapp/activities/ViewAccountActivity.java
+++ app/src/main/java/nz/emissary/emissaryapp/activities/ViewAccountActivity.java
@@ -5,13 +5,18 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
 
 import nz.emissary.emissaryapp.R;
+import nz.emissary.emissaryapp.User;
 
 /**
  * Created by Simon on 3/03/2016.
@@ -20,9 +25,13 @@ public class ViewAccountActivity extends AppCompatActivity {
 
     TextView usernameView;
     TextView emailView;
+    TextView firstName;
+    TextView lastName;
+
     Button logoutButton;
 
     Firebase mRef;
+    User user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,8 +49,25 @@ public class ViewAccountActivity extends AppCompatActivity {
         usernameView = (TextView) findViewById(R.id.username);
         emailView = (TextView) findViewById(R.id.email);
         logoutButton = (Button) findViewById(R.id.logout_button);
+        firstName = (TextView) findViewById(R.id.first_name);
+        lastName = (TextView) findViewById(R.id.last_name);
 
         emailView.setText(mRef.getAuth().getProviderData().get("email").toString());
+
+        Firebase currentFirebaseUser = new Firebase("https://emissary.firebaseio.com/users/" + mRef.getAuth().getUid());
+        currentFirebaseUser.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                user = dataSnapshot.getValue(User.class);
+                firstName.setText(user.getFirstName());
+                lastName.setText(user.getLastName());
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
+            }
+        });
 
         logoutButton.setOnClickListener(new View.OnClickListener() {
             @Override
