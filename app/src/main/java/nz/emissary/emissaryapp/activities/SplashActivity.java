3a411b4b802aaa5f8c01fc6747596572f558f1c1
Simon du Preez <smdupnz@yahoo.co.nz>
2016-04-10 17:06:27 +1200

    Changed splash screen and removed a bunch of files

---
+++ app/src/main/java/nz/emissary/emissaryapp/activities/SplashActivity.java
@@ -0,0 +1,47 @@
+package nz.emissary.emissaryapp.activities;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.TextView;
+
+import nz.emissary.emissaryapp.R;
+
+public class SplashActivity extends Activity implements View.OnTouchListener{
+
+    private final int SPLASH_DISPLAY_LENGTH = 15000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        TextView tv = (TextView) findViewById(R.id.logo_text_drawable);
+        Typeface face = Typeface.createFromAsset(getAssets(),
+                "fonts/emissary_font_main.ttf");
+
+        tv.setTypeface(face);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent(SplashActivity.this, HomeActivity.class);
+                SplashActivity.this.startActivity(mainIntent);
+                SplashActivity.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        Intent mainIntent = new Intent(SplashActivity.this, HomeActivity.class);
+        SplashActivity.this.startActivity(mainIntent);
+        SplashActivity.this.finish();
+        return false;
+    }
+}
